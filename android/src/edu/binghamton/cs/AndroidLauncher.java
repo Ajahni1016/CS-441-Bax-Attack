886397cb3427d4a148a8d0702b4b641119dc9918
ajahni1016 <alee884@binghamton.edu>
2021-11-08 00:41:45 -0500

    Accidentally committed all code from last week to the wrong repository. Merging.

--- android/src/edu/binghamton/cs/AndroidLauncher.java
+++ android/src/edu/binghamton/cs/AndroidLauncher.java
@@ -1,28 +1,16 @@
 package edu.binghamton.cs;
 
 import android.os.Bundle;
+
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import edu.binghamton.cs.CustomDrawing;
+import edu.binghamton.cs.BaxAttack;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		//initialize(new CustomDrawing(), config);
-	}
-
-	@Override
-	protected void onResume() {
-		super.onResume();
-
-		// ARCore requires camera permission to operate.
-		if (!CameraPermissionHelper.hasCameraPermission(this)) {
-			CameraPermissionHelper.requestCameraPermission(this);
-			return;
-		}
-
-  …
+		initialize(new BaxAttack(), config);
 	}
 }
--- android/src/edu/binghamton/cs/CameraPermissionHelper.java
+++
@@ -1,5 +0,0 @@
-package edu.binghamton.cs;
-
-public class CameraPermissionHelper
-{
-}
